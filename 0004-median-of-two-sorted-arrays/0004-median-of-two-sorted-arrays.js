1b542d6ac75e51c7e699dcad17bc948cde5485be
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-03 11:12:19 +0900

    Time: 87 ms (98.59%), Space: 47.9 MB (46.49%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,37 +4,13 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    const mergeArr=nums1.concat(nums2);
-    
-    function quickSort(arr){
-        const left=[];
-        const right=[];
-        if(arr.length<=1){
-            return arr;
-        }
-        const mid=Math.floor(arr.length/2);
-        const midArr=arr.splice(mid,1);
-        
-        for(let i=0; i<arr.length; i++){
-            if(arr[i]<midArr[0]){
-                left.push(arr[i]);
-            }else if(arr[i]>midArr[0]){
-                right.push(arr[i]);
-            }else{
-                midArr.push(arr[i]);
-            }
-        }
-        
-        return quickSort(left).concat(midArr,quickSort(right));
-    }
-    const sortedArr=quickSort(mergeArr);
-    
-    const length=sortedArr.length;
-    const mid=Math.floor(length/2);
-    console.log(sortedArr);
-    if(length%2!==0){
-        return sortedArr[mid];
-    }else{
-        return (sortedArr[mid]+sortedArr[mid-1])/2;
-    }
+    const sortedArr=nums1.concat(nums2).sort((a,b)=>a-b);
+    const length = sortedArr.length;
+  const mid = Math.floor(length / 2);
+
+  if (length % 2 !== 0) {
+    return sortedArr[mid];
+  } else {
+    return (sortedArr[mid] + sortedArr[mid - 1]) / 2;
+  }
 };
