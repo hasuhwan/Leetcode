c09d9c2e8b69fa1554adc2afaeee3cd536fd5254
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-05-30 09:24:12 +0900

    Time: 73 ms (83.99%), Space: 46.3 MB (92.23%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -6,25 +6,24 @@
 var findOrder = function(numCourses, prerequisites) {
     const answer=[];
     const arr=new Array(numCourses).fill(0).map(()=>[]);
-    const visited=new Array(numCourses).fill(false);
+const count=new Array(numCourses).fill(0);
     prerequisites.forEach(([after,before])=>{
-        arr[after].push(before);
+        arr[before].push(after);
+        count[after]++;
     })
 const queue=[];
-arr.forEach((el,idx)=>{
-    if(el.length===0){
+count.forEach((el,idx)=>{
+    if(el===0){
         queue.push(idx);
-        visited[idx]=true;
     }
-});
+})
     while(queue.length!==0){
         const temp=queue.shift();
         answer.push(temp);
-        arr.forEach((el,idx)=>{
-            arr[idx]=arr[idx].filter(el=>el!==temp);
-            if(arr[idx].length===0&&visited[idx]===false){
-                queue.push(idx);
-                visited[idx]=true;
+        arr[temp].forEach(el=>{
+            count[el]--;
+            if(count[el]===0){
+                queue.push(el);
             }
         })
     }
