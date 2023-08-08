aa1f56d1a53403ba626d9a0385b1b144bad6330b
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-08-08 16:37:21 +0900

    Time: 139 ms (99.61%), Space: 54.3 MB (87.50%) - LeetHub

--- 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.js
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.js
@@ -4,10 +4,7 @@
  */
 var findValueOfPartition = function(nums) {
     let answer=Infinity;
-    const set=new Set(nums);
-    if(nums.length!==set.size){
-        return 0;
-    }
+   
     nums.sort((a,b)=>a-b);
     for(let i=0; i<nums.length-1; i++){
         answer=Math.min(answer,Math.abs(nums[i]-nums[i+1]));
