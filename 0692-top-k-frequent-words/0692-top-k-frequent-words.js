69ffcbae5f543ee13607d1ef7b27efe4682589e2
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-11 15:41:44 +0900

    Time: 100 ms (6.91%), Space: 48.1 MB (17.82%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    const obj={};
+    words.forEach((str)=>{
+        if(obj[str]){
+            obj[str]++;
+        }else{
+            obj[str]=1;
+        }
+    });
+    return Object.entries(obj).sort((a,b)=>{
+        if(a[1]===b[1]){
+            console.log(b[0]);
+            console.log(a[0]);
+            return a[0]<b[0]?-1:1;
+        }else{
+            return b[1]-a[1];
+        }
+    }).map(el=>el[0]).slice(0,k);
+};
