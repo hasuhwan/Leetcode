1461f5e1b1a020278b7908ee1fbe8d48bb68fc4e
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-10-23 14:35:48 +0900

    Time: 43 ms (92.72%), Space: 41.9 MB (41.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var rob = function(nums) {
+    const dp=new Array(nums.length).fill(0);
     if(nums.length===1){
         return nums[0];
     }
-    const dp=new Array(nums.length).fill(0);
+        
     dp[0]=nums[0];
-    dp[1]=Math.max(nums[1],nums[0]);
-    
+    dp[1]=Math.max(dp[0],nums[1]);
     for(let i=2; i<dp.length; i++){
         dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
     }
-    console.log(dp);
-    return Math.max(...dp);
+    return dp[dp.length-1];
 };
