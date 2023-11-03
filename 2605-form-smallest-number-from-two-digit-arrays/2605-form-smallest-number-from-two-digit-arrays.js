cab30304b1d49f59f3eda180d4d62db41e131763
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-11-03 15:35:30 +0900

    Time: 56 ms (57.14%), Space: 43.5 MB (19.25%) - LeetHub

---
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var minNumber = function(nums1, nums2) {
+    nums1.sort((a,b)=>a-b);
+    nums2.sort((a,b)=>a-b);
+    for(let i=0; i<nums1.length; i++){
+        if(nums2.includes(nums1[i])){
+            return nums1[i];
+        }
+    }
+    return nums1[0]>nums2[0]?String(nums2[0])+String(nums1[0]):String(nums1[0])+String(nums2[0])
+};
