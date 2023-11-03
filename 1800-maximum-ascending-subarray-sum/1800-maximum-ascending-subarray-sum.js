8ae94ac6a09fabd68a18eb519a9c07e1bb98e982
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-11-03 17:10:25 +0900

    Time: 42 ms (95.88%), Space: 42.1 MB (40.21%) - LeetHub

---
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+    let max=nums[0];
+    let temp=nums[0]
+    for(let i=1; i<nums.length; i++){
+        if(nums[i]>nums[i-1]){
+            temp+=nums[i];
+        }else{
+            temp=nums[i];
+        }
+        max=Math.max(max,temp);
+    }
+    return max;
+};
