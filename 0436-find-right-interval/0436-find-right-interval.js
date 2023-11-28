fbbc0d7ae3e4ac882d82e4786667f6be1fa52325
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-11-28 16:03:27 +0900

    Time: 97 ms (75.51%), Space: 51 MB (31.63%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.js
+++ 0436-find-right-interval/0436-find-right-interval.js
@@ -1,18 +1,36 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[]}
+ */
 var findRightInterval = function(intervals) {
-    const starts = intervals.map(([a, b], j) => [a, j]).sort(([a], [b]) => a - b)
-    const res = Array(intervals.length).fill(-1);
-    
-    for(let i = 0; i < intervals.length; i++) {
-        const curr = intervals[i][1];
-        let left = 0, right = starts.length-1;
-        
-        while(left <= right) {
-            const mid = Math.floor((left + right)/2);
-            const [val, idx] = starts[mid];
-            if(val == curr) { res[i] = idx; break; }
-            if(val > curr) { res[i] = idx; right = mid - 1; } 
-            else left = mid + 1;
+    const n=intervals.length;
+    const answer=new Array(n).fill(-1);
+    const map=new Map();
+    const sorted=intervals.map((el,idx)=>[el[0],idx]).sort((a,b)=>a[0]-b[0]);
+    console.log(sorted);
+    intervals.forEach(([start,end],i)=>{
+        if(map.has(end)){
+            answer[i]=map.get(end);
+        }else{
+            let left=0;
+            let right=n-1;
+            let mid;
+            while(left<=right){
+                mid=Math.floor((left+right)/2);
+                const [curr,idx]=sorted[mid];
+                if(curr===end){
+                    map.set(end,idx);
+                    answer[i]=idx;
+                    return;
+                }else if(curr>end){
+                    map.set(end,idx);
+                    answer[i]=idx;
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
         }
-    }
-    return res;
+    });
+    return answer;
 };
