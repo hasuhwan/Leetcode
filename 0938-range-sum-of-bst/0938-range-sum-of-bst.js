c6345a026e72065508a67ae5dc831d68260ab6ea
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-12-26 14:28:21 +0900

    Time: 132 ms (35.69%), Space: 93.4 MB (97.90%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.js
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -13,13 +13,32 @@
  * @return {number}
  */
 var rangeSumBST = function(root, low, high) {
-    function dfs(node){
-        if(!node){
-        return 0;
+    let answer=0;
+    const queue=[root];
+    while(queue.length!==0){
+        const node=queue.shift();
+        if(node.val>=low&&node.val<=high){
+            answer+=node.val;
+        }
+        if(node.left){
+            queue.push(node.left);
+        }
+        if(node.right){
+            queue.push(node.right);
+        }
     }
-    const val=node.val>=low&&node.val<=high?node.val:0;
-    
-    return val+dfs(node.left)+dfs(node.right);
-    }
-    return dfs(root);
+    return answer;
 };
+
+
+// var rangeSumBST = function(root, low, high) {
+//     function dfs(node){
+//         if(!node){
+//         return 0;
+//     }
+//     const val=node.val>=low&&node.val<=high?node.val:0;
+    
+//     return val+dfs(node.left)+dfs(node.right);
+//     }
+//     return dfs(root);
+// };
