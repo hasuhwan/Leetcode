b915e95c7c34fee716c7fe4ab5ffc15aee7f1b72
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-12-27 12:46:30 +0900

    Time: 66 ms (38.84%), Space: 43.1 MB (83.50%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.js
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.js
@@ -7,8 +7,6 @@ var checkAlmostEquivalent = function(word1, word2) {
     const map=new Map();
     for(let i=0; i<word1.length; i++){
         map.set(word1[i],(map.get(word1[i])||0)+1);
-    }
-    for(let i=0; i<word2.length; i++){
         map.set(word2[i],(map.get(word2[i])||0)-1);
     }
     for(let val of map.values()){
