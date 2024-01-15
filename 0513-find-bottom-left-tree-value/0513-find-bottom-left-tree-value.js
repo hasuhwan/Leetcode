0dd3ef5fa32a31c6f41d45ad29f81f350864fbb1
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-01-16 08:48:49 +0900

    Time: 59 ms (80.21%), Space: 49.9 MB (5.21%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+    const map=new Map();
+    const queue=[[0,root]];
+    while(queue.length!==0){
+        const [dep,node]=queue.shift();
+        if(!node){
+            continue;
+        }
+        if(!map.has(dep)){
+            map.set(dep,[]);
+        }
+        map.get(dep).push(node.val);
+        queue.push([dep+1,node.left]);
+        queue.push([dep+1,node.right]);
+    }
+    return map.get(map.size-1)[0];
+};
