ecf4a7a939eb5ceea7b39f7d692191a2a5bfae68
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-01-18 15:50:29 +0900

    Time: 54 ms (46.48%), Space: 42.4 MB (30.44%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    function dfs(node1,node2){
+        if(!node1&&!node2){
+            return true;
+        }
+        if(!node1||!node2){
+            return false;
+        }
+        if(node1.val===node2.val){
+            if(!dfs(node1.left,node2.left)){
+                return false;
+            }
+            if(!dfs(node1.right,node2.right)){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    return dfs(p,q);
+};
