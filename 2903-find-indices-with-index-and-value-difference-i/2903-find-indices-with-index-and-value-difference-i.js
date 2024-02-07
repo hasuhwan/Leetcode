da5ef051e9413e3c61b1e83ef67647ac902d940c
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-02-07 13:08:22 +0900

    Time: 63 ms (50.48%), Space: 50.6 MB (11.43%) - LeetHub

---
+++ 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} indexDifference
+ * @param {number} valueDifference
+ * @return {number[]}
+ */
+var findIndices = function(nums, indexDifference, valueDifference) {
+    for(let i=0; i<nums.length-indexDifference; i++){
+        for(let j=i+indexDifference; j<nums.length; j++){
+            if(Math.abs(nums[i]-nums[j])>=valueDifference){
+                return [i,j];
+            }
+        }
+    }
+    return [-1,-1];
+};
