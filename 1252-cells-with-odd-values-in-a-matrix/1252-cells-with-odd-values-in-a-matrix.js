8000b20dd59b3b389597b5e7198560345e8025f7
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-03-04 16:26:11 +0900

    Time: 67 ms (33.63%), Space: 50.9 MB (61.95%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function(m, n, indices) {
+    let answer=0;
+    const boolArr=new Array(m).fill(1);
+    const rowArr=new Array(m).fill(1);
+    const colArr=new Array(n).fill(1);
+    indices.forEach(([row,col])=>{
+        rowArr[row]*=-1;
+        colArr[col]*=-1;
+    });
+    rowArr.forEach(row=>{
+        colArr.forEach(col=>{
+            if(row*col===-1){
+                answer++;
+            }
+        })
+    });
+    return answer;
+};
